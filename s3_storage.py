9e35379d4a0d12ef9dd9eb11babc88f6af652f9e
Matija Slivonja <matija.slivonja@gmail.com>
2025-08-10 21:18:02 +0200

    Update S3 storage service implementation, added endpoint
    Add endpoint used for S3 client configuration
    Implement generation of temp url valid for 1h

--- s3_storage.py
+++ s3_storage.py
@@ -2,24 +2,34 @@ import boto3
 import io
 
 class S3Service:
-    def __init__(self, access_key, secret_key, bucket_name, region_name):
+    def __init__(self, endpoint, access_key, secret_key, bucket_name, region_name = 'us-east-1'):
         self.bucket_name = bucket_name
         self.region_name = region_name
         self.s3_client = boto3.client(
             "s3",
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-            region_name=region_name
+            endpoint_url = endpoint,
+            aws_access_key_id = access_key,
+            aws_secret_access_key = secret_key,
+            region_name = region_name
         )
 
     def upload_file(self, file_content:str, file_name:str):
         # Convert string to bytes for upload
         file_obj = io.BytesIO(file_content.encode("utf-8"))
+
+        # Upload object
         self.s3_client.upload_fileobj(
             file_obj,
             self.bucket_name,
             file_name,
             ExtraArgs={"ContentType": "text/csv", "ACL": "public-read"}
         )
-        return f"http://localhost:9000/{self.bucket_name}/{file_name}"
+
+        # Generate URL valid for 1 hour
+        url = self.s3_client.generate_presigned_url(
+            ClientMethod="get_object",
+            Params={"Bucket": self.bucket_name, "Key": file_name},
+            ExpiresIn=3600  # seconds
+        )
+        return url
 
